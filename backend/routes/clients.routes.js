b4d29a8901e11828dc2e3a5e4b14d3eecb486249
ArmandC <diegoarmandocanaviremamani@gmail.com>
2026-05-04 08:55:05 -0500

    Add authentication middleware to client routes

--- backend/routes/clients.routes.js
+++ backend/routes/clients.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/auth.middleware')
 const clientsController = require('../controllers/clientsController')
 
-router.get('/', clientsController.getClients)
-router.post('/', clientsController.createClient)
-router.put('/:id', clientsController.updateClient)
-router.delete('/:id', clientsController.deleteClient)
+router.get('/', auth, clientsController.getClients)
+router.post('/', auth, clientsController.createClient)
+router.put('/:id', auth, clientsController.updateClient)
+router.delete('/:id', auth, clientsController.deleteClient)
 
 module.exports = router
