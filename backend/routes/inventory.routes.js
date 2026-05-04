2d9aebc5d1f519645e2f9fe4e9860615ade963cf
ArmandC <diegoarmandocanaviremamani@gmail.com>
2026-05-04 08:55:41 -0500

    Add authentication middleware to inventory routes

--- backend/routes/inventory.routes.js
+++ backend/routes/inventory.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/auth.middleware')
 const inventoryController = require('../controllers/inventoryController')
 
-router.get('/', inventoryController.getInventory)
-router.post('/', inventoryController.createInventory)
-router.put('/:id', inventoryController.updateInventory)
-router.delete('/:id', inventoryController.deleteInventory)
+router.get('/', auth, inventoryController.getInventory)
+router.post('/', auth, inventoryController.createInventory)
+router.put('/:id', auth, inventoryController.updateInventory)
+router.delete('/:id', auth, inventoryController.deleteInventory)
 
 module.exports = router
