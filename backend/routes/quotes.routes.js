ed122205a57cb28e63ef18ff66be34dacad4d3cf
ArmandC <diegoarmandocanaviremamani@gmail.com>
2026-05-04 08:56:25 -0500

    Add authentication middleware to quotes routes

--- backend/routes/quotes.routes.js
+++ backend/routes/quotes.routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/auth.middleware')
 const quotesController = require('../controllers/quotesController')
 
-router.get('/', quotesController.getQuotes)
-router.post('/', quotesController.createQuote)
-router.delete('/:id', quotesController.deleteQuote)
+router.get('/', auth, quotesController.getQuotes)
+router.post('/', auth, quotesController.createQuote)
+router.delete('/:id', auth, quotesController.deleteQuote)
 
 module.exports = router
